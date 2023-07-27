5223347a47f16e2a08c3891715e7ce482cd43936
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-07-27 18:11:30 -0400

    Add platform behavior placeholders Ready to implement more later

--- Assets/Scripts/Platforms/PlatformsBehavior.cs
+++ Assets/Scripts/Platforms/PlatformsBehavior.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlatformsBehavior : MonoBehaviour
@@ -6,19 +7,34 @@ public class PlatformsBehavior : MonoBehaviour
     {
         Normal,
         Notgrappable,
-        Disappear,
+        Breakable,
         Uneven,
+        Moving,
         Rotating
     }
     public PlatformType type;
 
-    void Start()
-    {
-        
-    }
+    [Tooltip("Indicate whether this platform should activate its own script at the start or after being latched")]
+    public bool shouldActive;
+    public bool isLatched;
 
     void Update()
     {
-        
+        if (!shouldActive && isLatched) TypeHandler();
+    }
+
+    private void TypeHandler() 
+    { 
+        switch(type)
+        {
+            case PlatformType.Breakable:
+                break;
+            case PlatformType.Moving:
+                break;
+            case PlatformType.Rotating: 
+                break;
+            default:
+                break;
+        }
     }
 }
