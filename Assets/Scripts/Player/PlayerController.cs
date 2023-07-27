bba287e42808a35b0210da4cb70f36a793e15629
lejara <leoneljara3@gmail.com>
2023-07-26 21:37:27 -0400

    Add retract, tests and utils

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -18,10 +18,16 @@ public class PlayerController : MonoBehaviour
     public bool isGrappling;
     [ReadOnly]
     public bool isSwinging;
+    [ReadOnly]
+    public bool isRetracting;
     public float grappleTravelTime;
-    public float grappleDistance;
+    public float grappleRetractTime;
+    public float maxGrappleDistance;
     public LayerMask platfromLayer;
     public AnimationCurve grappleTravelCurve;
+    public AnimationCurve grappleRetractCurve;
+    [Tooltip("Growth scale for how much time to take off based on grapple length")]
+    public AnimationCurve grappleTimeRetractCurve;
 
 
     [Header("Swing Attributes")]
@@ -53,23 +59,31 @@ public class PlayerController : MonoBehaviour
 
         lineRenderer.enabled = false;
         lineRenderer.positionCount = 2;
+
+        //Tests. Make sure we have the correct hierarchy.
+        Debug.Assert(transform.GetChildsWhere((childT) => (childT == grappleStartPoint)).Count == 1,
+        "grappleStartPoint must be a transfom child of this object");
+
+        Debug.Assert(transform.GetChildsWhere((childT) => (childT == grappleEndPoint)).Count == 0,
+        "grappleEndPoint must not be a transfrom child of this object");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        lineRenderer.SetPosition(0, grappleStartPoint.position);
+
         if (gameState.state != States.Playing)
         {
             return;
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
-        lineRenderer.SetPosition(0, grappleStartPoint.position);
 
         #region Grapple
 
-
-        if (Input.GetKeyDown(KeyCode.Mouse0) && !isGrappling && !isSwinging)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isGrappling && !isSwinging && !isRetracting)
         {
             StartCoroutine(ShootGrapple());
         }
@@ -78,6 +92,12 @@ public class PlayerController : MonoBehaviour
             DetachGrapple();
         }
 
+        //It is assumed grappleEndPoint is not a transfrom child of this object.
+        //We need to keep it synced when its not in use
+        if (isLanded && !isGrappling && !isRetracting)
+        {
+            SetGrapplePosition(grappleStartPoint.position);
+        }
 
         #endregion
     }
@@ -111,6 +131,8 @@ public class PlayerController : MonoBehaviour
         /* Can have character flip here based on the direction of velocity. */
     }
 
+    #region Grapple Methods
+
     private IEnumerator ShootGrapple()
     {
 
@@ -118,13 +140,11 @@ public class PlayerController : MonoBehaviour
         lineRenderer.enabled = true;
 
         float normTime = 0;
-        Vector3 target = grappleStartPoint.position + new Vector3(0, grappleDistance, 0);
-        while (normTime < 1.0f &&
-                Vector3.Distance(grappleStartPoint.position, grappleEndPoint.position) < grappleDistance)
+        Vector3 target = grappleStartPoint.position + new Vector3(0, maxGrappleDistance, 0);
+        while (normTime < 1.0f && GetGrappleDistance() < maxGrappleDistance)
         {
             //Move Grapple
-            grappleEndPoint.position = Vector3.Lerp(grappleStartPoint.position, target, grappleTravelCurve.Evaluate(normTime));
-            lineRenderer.SetPosition(1, grappleEndPoint.position);
+            SetGrapplePosition(Vector3.Lerp(grappleStartPoint.position, target, grappleTravelCurve.Evaluate(normTime)));
 
             if (Physics.Raycast(grappleEndPoint.position, Vector3.up, out RaycastHit hit, 0.1f, platfromLayer))
             {
@@ -132,12 +152,12 @@ public class PlayerController : MonoBehaviour
                 yield break;
             }
 
-            // Edge case, let detach if the grapple will phase through a collider. 
-            // This can happend when the player is shooting to a non-valid platfrom
+            // Edge case, lets detach if the grapple will phase through a collider. 
+            // This can happend when the player is shooting to a non-valid platform
             if (Physics.Raycast(grappleStartPoint.position,
                 Vector3.up,
                 out RaycastHit _,
-                Vector3.Distance(grappleStartPoint.position, grappleEndPoint.position)))
+                GetGrappleDistance()))
             {
                 //MAYBE: could do some fancy animation based on the raycast hit
                 DetachGrapple();
@@ -146,7 +166,7 @@ public class PlayerController : MonoBehaviour
 
 
             normTime += Time.deltaTime / grappleTravelTime;
-            yield return null; //allow for an Update
+            yield return null;
         }
 
         DetachGrapple();
@@ -158,12 +178,14 @@ public class PlayerController : MonoBehaviour
         isSwinging = true;
         isLanded = false;
 
+        SetGrapplePosition(point);
+
         // Joint Setup
         joint = gameObject.AddComponent<SpringJoint>();
         joint.autoConfigureConnectedAnchor = false;
         joint.connectedAnchor = point;
 
-        float distance = Vector3.Distance(grappleStartPoint.position, point);
+        float distance = GetGrappleDistance();
         joint.maxDistance = 0.6f * distance;
         joint.minDistance = 0.3f * distance;
         joint.spring = 8f;
@@ -171,19 +193,59 @@ public class PlayerController : MonoBehaviour
         joint.massScale = 3f;
     }
 
+
     private void DetachGrapple()
     {
         isSwinging = false;
         isGrappling = false;
-        lineRenderer.enabled = false;
-        grappleEndPoint.position = grappleStartPoint.position;
-        lineRenderer.SetPosition(1, grappleEndPoint.position);
-
         Destroy(joint);
+
+        StartCoroutine(RetractGrapple());
+    }
+
+
+    private IEnumerator RetractGrapple()
+    {
+        isRetracting = true;
+
+        float normTime = 0;
+        //Lets scale down the time based on the length of the grapple
+        float totalTime = grappleTimeRetractCurve.Evaluate(GetGrappleDistance() / maxGrappleDistance) * grappleRetractTime;
+        Vector3 startingPosition = grappleEndPoint.position;
+
+        while (normTime < 1.0f)
+        {
+            //Move Grapple
+            SetGrapplePosition(Vector3.Lerp(startingPosition, grappleStartPoint.position, grappleRetractCurve.Evaluate(normTime)));
+
+            normTime += Time.deltaTime / totalTime;
+            yield return null;
+        }
+
+        lineRenderer.enabled = false;
+        isRetracting = false;
+
     }
 
     private void Swing(float horizontalInput)
     {
         playerRb.AddForce(horizontalInput * horizontalForce * Vector3.right, ForceMode.Force);
     }
+
+
+    #endregion
+
+    #region Utils
+
+    private float GetGrappleDistance()
+    {
+        return Vector3.Distance(grappleStartPoint.position, grappleEndPoint.position);
+    }
+
+    private void SetGrapplePosition(Vector3 pos)
+    {
+        grappleEndPoint.position = pos;
+        lineRenderer.SetPosition(1, grappleEndPoint.position);
+    }
+    #endregion
 }
