fcc615493ad0c6cd93d48d4a0fbda424699c0107
lejara <leoneljara3@gmail.com>
2023-07-30 11:53:08 -0400

    Refractor and rename prefabs

--- Assets/Scripts/Menus/MenuInput.cs
+++ Assets/Scripts/Menus/MenuInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class MainMenuInput : MonoBehaviour
+public class MenuInput : MonoBehaviour
 {
 
     public UnityEvent OnExitMenus;
